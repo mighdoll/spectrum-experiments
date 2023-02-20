1567198df07826f144fac2e46b67f60c654bc994
lee mighdoll <lee@underneath.ca>
2023-02-20 15:05:41 -0800

    experiments with dialog

--- src/Imports.ts
+++ src/Imports.ts
@@ -1,1 +1,11 @@
-import '@spectrum-web-components/bundle/elements.js';
+// import '@spectrum-web-components/bundle/elements.js';
+// import '@spectrum-web-components/theme/sp-theme.js';
+// import '@spectrum-web-components/theme/src/themes.js';
+// import '@spectrum-web-components/button/sp-button.js';
+// import '@spectrum-web-components/dialog/sp-dialog-wrapper.js';
+
+
+import '@spectrum-web-components/theme/sp-theme.js';
+import '@spectrum-web-components/theme/src/themes.js';
+import '@spectrum-web-components/button/sp-button.js';
+import '@spectrum-web-components/dialog/sp-dialog-wrapper.js';
