79a175c28138655d24cc8b41236e1d2be87bca8c
lee mighdoll <lee@underneath.ca>
2023-02-20 15:05:41 -0800

    slider example

--- src/App.ts
+++ src/App.ts
@@ -1,10 +1,7 @@
-import Sortable from "sortablejs";
 
 main();
 
 export function main():void {
   console.clear();
   console.log("hello");
-  const el = document.querySelector("sp-accordion")!;
-  new Sortable(el, {animation: 300});
 }
