78370cd623f4a21e4568601c4dffb1fa713adfef
lee mighdoll <lee@underneath.ca>
2023-02-20 15:59:42 -0800

    slider inside histogram

--- src/Imports.ts
+++ src/Imports.ts
@@ -1,4 +1,4 @@
-// import '@spectrum-web-components/bundle/elements.js';
+import '@spectrum-web-components/bundle/elements.js';
 // import '@spectrum-web-components/theme/sp-theme.js';
 // import '@spectrum-web-components/theme/src/themes.js';
 // import '@spectrum-web-components/button/sp-button.js';
@@ -9,4 +9,4 @@ import '@spectrum-web-components/theme/sp-theme.js';
 import '@spectrum-web-components/theme/src/themes.js';
 // import '@spectrum-web-components/button/sp-button.js';
 // import '@spectrum-web-components/dialog/sp-dialog-wrapper.js';
-import '@spectrum-web-components/slider/sp-slider.js';
+// import '@spectrum-web-components/slider/sp-slider.js';
