8790b0f9b2eadbe290670690b6f3a55a935cd05d
lee mighdoll <lee@underneath.ca>
2023-02-20 15:05:41 -0800

    sortable accordion works

--- src/Imports.ts
+++ src/Imports.ts
@@ -1,4 +1,4 @@
-// import '@spectrum-web-components/bundle/elements.js';
+import '@spectrum-web-components/bundle/elements.js';
 // import '@spectrum-web-components/theme/sp-theme.js';
 // import '@spectrum-web-components/theme/src/themes.js';
 // import '@spectrum-web-components/button/sp-button.js';
